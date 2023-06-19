f9561078ad481fbdc3457fb62b29d80c2b014a2c
Sierra Spencer <31259321+SierraSpencer@users.noreply.github.com>
2023-06-18 21:14:10 -0700

    Create root.jsx

---
+++ src/routes/root.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import Header from '../components/header/header';
+import Nav from '../components/nav/nav';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { fetchMyProfile } from '../api';
+
+function Root() 
+{
+  const [token, setToken] = useState('');
+  const [myProfile, setMyProfile] = useState({});
+  const [allRoutines, setAllRoutines] = useState([]);
+  const [allActivities, setAllActivities] = useState([]);
+  useEffect(() => {
+    if (localStorage.getItem('token')) 
+    {
+      setToken(localStorage.getItem('token'));
+    }
+    if (token !== '') 
+    {
+      (async () => {
+        const profileValues = await fetchMyProfile(token);
+        setMyProfile(profileValues);
+      })();
+    }
+  }, [token]);
+  return (
+    <>
+      <Header myProfile={myProfile} />
+      <Nav myProfile={myProfile} setMyProfile={setMyProfile} />
+      <div id="main">
+        <Outlet
+          context={{
+            allRoutines,
+            setAllRoutines,
+            allActivities,
+            setAllActivities,
+            token,
+            setToken,
+            myProfile,
+            setMyProfile,
+          }}
+        />
+      </div>
+    </>
+  );
+}
+export default Root;
