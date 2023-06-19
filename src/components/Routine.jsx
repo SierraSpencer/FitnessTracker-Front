5089f9990ee1d709041b4cc06204017f8b93ab6a
Sierra Spencer <31259321+SierraSpencer@users.noreply.github.com>
2023-06-18 22:12:15 -0700

    Create Routine.jsx

---
+++ src/components/Routine.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate, useOutletContext, useParams} from "react-router-dom";
+
+export default function Routine() 
+{
+    const { routineId } = useParams();
+    const { routines, user } = useOutletContext()
+    const routine = routines.find((routine) => routine.id == routineId);
+    console.log(routine)
+    if(!routine)  
+    {
+        return <></>
+    }
+
+    return ( 
+    <div>
+      <h1>Routine</h1>
+      <div>Name: {routine.name}</div>
+      <div>Goal: {routine.goal}</div>
+      <div>Creator: {routine.creatorName}</div>
+      {routine.creatorName === user.username && (<><Link to={`/routines/${routine.id}/edit`} >Edit Routine</Link>
+      )} {routine.activities.map(activity => 
+      <div key={activity.id}>
+        <div>Activity: {activity.name}</div>
+        <div>Description: {activity.description}</div>
+        <div>Duration: {activity.duration}</div>
+        <div>Count: {activity.count}</div>
+      </div>
+      )}
+    </div>
+    )
+}
