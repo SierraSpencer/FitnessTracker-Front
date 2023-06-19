2549629ab53e5c1867af66887266deb0386f8344
Sierra Spencer <31259321+SierraSpencer@users.noreply.github.com>
2023-06-18 22:32:03 -0700

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react';
+import React from "react";
+import { useState } from 'react';
 import { registerUser, loginUser } from '../../api';
 import { useNavigate, useOutletContext } from 'react-router';
 
-const Login = () => 
+export default function Login()
 {
   const [registerUsername, setUsername] = useState('');
   const [registerPassword, setPassword] = useState('');
@@ -13,9 +14,9 @@ const Login = () =>
   const [loginFormError, LoginFormError] = useState();
   const navigate = useNavigate();
   
-  async function handleRegister(e)
+  async function handleRegister(event)
   {
-    e.preventDefault();
+    event.preventDefault();
     const newUser = await registerUser(registerUsername, registerPassword);
     if (!newUser.user) 
     {
@@ -25,12 +26,12 @@ const Login = () =>
     console.log(newUser);
     localStorage.setItem('token', newUser.token);
     setToken(newUser.token);
-    navigate('/my-routines');
+    navigate('/myroutines');
   }
   
-  async function handleLogin(e) 
+  async function handleLogin(event) 
   {
-    e.preventDefault();
+    event.preventDefault();
     const loggedInUser = await loginUser(loginUsername, loginPassword);
     if (!loggedInUser.user) 
     {
@@ -40,26 +41,25 @@ const Login = () =>
     console.log(loggedInUser);
     localStorage.setItem('token', loggedInUser.token);
     setToken(loggedInUser.token);
-    navigate('/my-routines');
+    navigate('/myroutines');
   }
   
   return (
     <>
       <h3>Register</h3>
       <form id="register-form" onSubmit={handleRegister}>
-        <input placeholder="Create Username" type="text" value={registerUsername} onChange={(e) => setUsername(e.target.value)}/>
-        <input placeholder="Create Password" type="password" value={registerPassword} onChange={(e) => setPassword(e.target.value)}/>
+        <input placeholder="Create Username" type="text" value={registerUsername} onChange={(event) => setUsername(event.target.value)}/>
+        <input placeholder="Create Password" type="password" value={registerPassword} onChange={(event) => setPassword(event.target.value)}/>
         <button type="submit">Create New User</button>
         {formError}
       </form>
       <h3>Login</h3>
       <form id="login-form" onSubmit={handleLogin}>
-        <input placeholder="Username" type="text" value={loginUsername} onChange={(e) => setLoginUsername(e.target.value)}/>
-        <input placeholder="Password" type="password" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)}/>
+        <input placeholder="Username" type="text" value={loginUsername} onChange={(event) => setLoginUsername(event.target.value)}/>
+        <input placeholder="Password" type="password" value={loginPassword} onChange={(event) => setLoginPassword(event.target.value)}/>
         <button type="submit">Login</button>
         {loginFormError}
       </form>
     </>
   );
 };
-export default Login;
