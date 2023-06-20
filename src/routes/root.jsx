d768f539272bf35cd8f08551dd03c88354333311
Sierra Spencer <31259321+SierraSpencer@users.noreply.github.com>
2023-06-19 20:53:20 -0700

    Update root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import Nav from '../components/nav';
+import Nav from '../components/Nav';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { fetchMyProfile } from '../api';
 
