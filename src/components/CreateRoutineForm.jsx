d994453351a5ce728f8a82c3ddcb21ccdb714067
Sierra Spencer <31259321+SierraSpencer@users.noreply.github.com>
2023-06-19 21:41:09 -0700

    Update and rename RoutineForm.jsx to CreateRoutineForm.jsx

--- src/components/CreateRoutineForm.jsx
+++ src/components/CreateRoutineForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
 
-const RoutineForm = ({
+const CreateRoutineForm = ({
   createWindowOpen,
   createRoutine,
   getAllRoutines,
