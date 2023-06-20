b4baefba67849e7e9f538ed3298b3c2936522fe1
Sierra Spencer <31259321+SierraSpencer@users.noreply.github.com>
2023-06-19 20:59:28 -0700

    Update Activities.jsx

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from 'react';
-import { postActivities } from '../../api';
+import { postActivities } from '../api';
 import { useOutletContext } from 'react-router';
 
 export default function Activities() 
