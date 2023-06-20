da2d20bbef8d526f95ab342de4b58596864edb00
Sierra Spencer <31259321+SierraSpencer@users.noreply.github.com>
2023-06-19 21:18:28 -0700

    Update MyRoutines.jsx

--- src/components/MyRoutines.jsx
+++ src/components/MyRoutines.jsx
@@ -1,61 +1,157 @@
 import { useNavigate, useOutletContext } from 'react-router';
-import { seeUserPublicRoutines, deleteRoutine } from '../api';
-import { useState } from 'react';
+import {
+  seeUserPublicRoutines,
+  deleteRoutine,
+  getAllRoutines,
+  createRoutine,
+} from '../../api';
+import { useEffect, useState } from 'react';
+import './myRoutines.css';
+import PopUpEdit from '../utils/popUpEdit';
+import CreateRoutineForm from '../routines/routinesSubcomoponents/createRoutineForm';
 
-export default function MyRoutines()
+export default MyRoutines() 
 {
   const { myProfile, token, setAllRoutines } = useOutletContext();
   const [userRoutines, setUserRoutines] = useState([]);
   const [editMode, setEditMode] = useState('');
   const [editRoutine, setEditRoutine] = useState({});
+  const [createWindowOpen, setCreateWindowOpen] = useState(false);
+
   const navigate = useNavigate();
+
   useEffect(() => {
-    if (token && myProfile.id) 
-    {
+    if (token && myProfile.id) {
       (async () => {
         const routines = await seeUserPublicRoutines(token, myProfile.username);
         setUserRoutines(routines);
       })();
     }
   }, [myProfile]);
-  const handleDelete = async (routineId) => 
-  {
+
+  const handleDelete = async (routineId) => {
     await deleteRoutine(token, routineId);
     const routines = await seeUserPublicRoutines(token, myProfile.username);
     setUserRoutines(routines);
   };
 
   return (
-    <div>
-      <h1>{user.username}'s Profile</h1>
-      <Link to="/newroutine">Add Routine</Link>
-      <h2>My Routines</h2>
-      {myRoutines.map((routine) => {
-      if(routine.creatorName === user.username || !routine.isPublic) 
-      {
-        return (
-          <div key={routine.id}>
-              <Link to={`/routines/${routine.id}`}>
-              <div>Name: {routine.name}</div></Link>
-              <div>Goal: {routine.goal}</div>
-              <div>Creator: {routine.creatorName}</div>
-              <button>Edit Routine</button>
-              <button>Delete Routine</button>
-              {!routine.isPublic && (<><button>Make Public</button></>)}
-              {!routine.activity && (<><button>Add Activity</button></>)}
-              {routine.activities.map(activity => 
-              <div key={activity.id}>
-              <div>Activity: {activity.name}</div>
-              <div>Description: {activity.description}</div>
-              <div>Duration: {activity.duration}</div>
-              <div>Count: {activity.count}</div>
-              <button>Add Activity</button>
-              <button>Edit Activity</button>
-              <button>Delete Activity</button>
-              </div>
-              )}
-          </div>
-         )}
-        })}
-    </div>)
-  }
+    <>
+      <div id="my-routines-body">
+        <h2>Welcome to YOUR routines, {myProfile.username}!</h2>
+        <p>
+          Here you can browse all your routines, make changes to them, or even
+          delete them if you no longer want or need them.
+        </p>
+        <p>
+          {'('}Tip: If you want to keep your routine, but dont want other users
+          to be able to see it, set it to 'Private' and it will no longer be
+          available to other users.{')'}
+        </p>
+        {userRoutines[0] ? (
+          <>
+            <h3>Your Routines:</h3>
+            <p>
+              Don't See what you're looking for?{' '}
+              <span
+                className="click-here"
+                onClick={() => setCreateWindowOpen(true)}
+              >
+                Click here
+              </span>{' '}
+              to create a new routine!
+            </p>
+          </>
+        ) : (
+          <h4>
+            You do not have any routines yet.{' '}
+            <span
+              className="click-here"
+              onClick={() => setCreateWindowOpen(true)}
+            >
+              Click here
+            </span>{' '}
+            to add some!
+          </h4>
+        )}
+        <CreateRoutineForm
+          createWindowOpen={createWindowOpen}
+          createRoutine={createRoutine}
+          getAllRoutines={getAllRoutines}
+          setAllRoutines={setAllRoutines}
+          token={token}
+        />
+        <div id="my-routines-list">
+          {userRoutines[0] &&
+            userRoutines.map((routine) => {
+              console.log(routine);
+              return (
+                <div className="my-routine-cards" key={routine.id}>
+                  <h4>{routine.name}</h4>
+                  <p>Goal: {routine.goal}</p>
+                  <p>Privacy: {routine.isPublic ? 'Public' : 'Private'}</p>
+                  <p>
+                    Activities Included:{' '}
+                    {routine.activities[0]
+                      ? routine.activities
+                          .map((activity) => activity.name)
+                          .join(', ')
+                      : 'None Yet, add some below!'}
+                  </p>
+
+                  <div className="my-routine-button-bank">
+                    <button
+                      onClick={() => {
+                        setEditMode('Add Activity');
+                        setEditRoutine(routine);
+                      }}
+                    >
+                      Add activity to routine
+                    </button>
+                    {editMode === 'Add Activity' && (
+                      <PopUpEdit
+                        setEditMode={setEditMode}
+                        editMode={editMode}
+                        routine={editRoutine}
+                        setUserRoutines={setUserRoutines}
+                        token={token}
+                        myProfile={myProfile}
+                        setAllRoutines={setAllRoutines}
+                      />
+                    )}
+
+                    <button
+                      onClick={() => {
+                        setEditMode('Edit Routine');
+                        setEditRoutine(routine);
+                      }}
+                    >
+                      Edit Routine
+                    </button>
+                    {editMode === 'Edit Routine' && (
+                      <PopUpEdit
+                        setEditMode={setEditMode}
+                        editMode={editMode}
+                        routine={editRoutine}
+                        token={token}
+                        setUserRoutines={setUserRoutines}
+                        setAllRoutines={setAllRoutines}
+                        myProfile={myProfile}
+                      />
+                    )}
+
+                    <button onClick={() => handleDelete(routine.id)}>
+                      Delete
+                    </button>
+                    <button onClick={() => navigate(`/routines/${routine.id}`)}>
+                      See / Edit Activity Details
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </>
+  );
+};
