2c0b2e9ef740611e9c4dd3297a1506b74b1ff56a
Sierra Spencer <31259321+SierraSpencer@users.noreply.github.com>
2023-06-19 21:22:10 -0700

    Update Routines.jsx

--- src/components/Routines.jsx
+++ src/components/Routines.jsx
@@ -1,34 +1,98 @@
-import React from "react";
-import { Link, useOutletContext } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { getAllRoutines, createRoutine } from '../api';
+import './routines.css';
+import { useNavigate, useOutletContext } from 'react-router';
+import RoutinesHeader from './RoutinesHeader';
+import CreateRoutineForm from './RoutineForm';
 
-export default function AllRoutines() 
-{
-    const {routines} = useOutletContext();
+const Routines = () => {
+  const { allRoutines, setAllRoutines, myProfile, token } = useOutletContext();
+  const [createWindowOpen, setCreateWindowOpen] = useState(false);
+  const [displayedRoutines, setDisplayedRoutines] = useState([]);
 
-    return(
-     <div>
-      <h1 id="header">Routines</h1>
-      {routines.map((routine) => {
-        if(routine.isPublic) 
-        {
-        return(
-          <div key={routine.id}>
-            <Link to={`/routines/${routine.id}`}>
-            <div>Name: {routine.name}</div></Link>
-            <div>Goal: {routine.goal}</div>
-            <div>Creator: {routine.creatorName}</div>
+  const navigate = useNavigate();
 
-            {routine.activities.map(activity => 
-            <div key={activity.id}>
-              <div>Activity: {activity.name}</div>
-              <div>Description: {activity.description}</div>
-              <div>Duration: {activity.duration}</div>
-              <div>Count: {activity.count}</div>
-            </div>
+  useEffect(() => {
+    (async () => {
+      const allRoutines = await getAllRoutines();
+      setAllRoutines(allRoutines);
+      setDisplayedRoutines(allRoutines);
+      console.log(allRoutines[0]);
+    })();
+  }, []);
+
+  const handleRoutineSelect = (routine) => {
+    navigate(`/routines/${routine.id}`);
+  };
+
+  return (
+    <>
+      <RoutinesHeader
+        allRoutines={allRoutines}
+        displayedRoutines={displayedRoutines}
+        setDisplayedRoutines={setDisplayedRoutines}
+        myProfile={myProfile}
+        createWindowOpen={createWindowOpen}
+        setCreateWindowOpen={setCreateWindowOpen}
+      />
+
+      <CreateRoutineForm
+        createWindowOpen={createWindowOpen}
+        createRoutine={createRoutine}
+        getAllRoutines={getAllRoutines}
+        setAllRoutines={setAllRoutines}
+        token={token}
+      />
+
+      <div id="routines-list">
+        {displayedRoutines.map((routine) => {
+          return (
+            <div className="routine-card" key={routine.id}>
+              <div className="routine-card-top">
+                <div className="routine-details">
+                  <h4 className="routine-name">{routine.name}</h4>
+                  <p className="routine-creator">
+                    Created by {routine.creatorName}
+                  </p>
+                  {myProfile.username === routine.creatorName && (
+                    <p className="your-routine-message">
+                      This is YOUR Routine!
+                    </p>
+                  )}
+                  <p className="routine-goal">Goal: {routine.goal}</p>
+                </div>
+                <div
+                  className="try-routine-button"
+                  onClick={() => handleRoutineSelect(routine)}
+                >
+                  Try{myProfile.username === routine.creatorName && '/Edit'}{' '}
+                  Now!
+                </div>
+              </div>
+              {routine.activities.length > 0 && (
+                <div>
+                  <p className="routine-activities-header">
+                    Activities Included:{' '}
+                  </p>
+
+                  <div className="routine-activity-bubble-container">
+                    {routine.activities.map((activity) => (
+                      <span
+                        key={activity.id}
+                        className="routine-activity-bubble"
+                      >
+                        {activity.name}
+                      </span>
+                    ))}
+                  </div>
+                </div>
               )}
-          </div>
-      );}})
-        }   
-    </div>
-    )
-}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Routines;
