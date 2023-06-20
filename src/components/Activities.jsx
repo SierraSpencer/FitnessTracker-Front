0b7e5b2bb38835a010086cfb8b360606d5044636
Sierra Spencer <31259321+SierraSpencer@users.noreply.github.com>
2023-06-19 21:02:05 -0700

    Update Activities.jsx

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,32 +1,46 @@
-import React from "react";
-import { useState } from 'react';
-import { postActivities } from '../api';
+import { useEffect, useState } from 'react';
+import { getAllActivities } from '../../api';
 import { useOutletContext } from 'react-router';
+import ActivitiesHeader from './activitesSubcomponents/activitiesHeader';
+import ActivityCreate from './activitesSubcomponents/activityCreate';
 
-export default function Activities() 
-{
-  const { allActivities, setAllActivities } = useOutletContext();
+const Activities = () => {
+  const { allActivities, setAllActivities, myProfile, token } =
+    useOutletContext();
+  const [displayedActivities, setDisplayedActivities] = useState([]);
+  const [createWindowOpen, setCreateWindowOpen] = useState(false);
 
   useEffect(() => {
-    (async () => 
-     {
-      const allActivities = await postActivities();
+    (async () => {
+      const allActivities = await getAllActivities();
       setAllActivities(allActivities);
-      console.log(allActivities[0]);
+      setDisplayedActivities(allActivities);
     })();
   }, []);
 
   return (
     <>
-      <p>Returning {allActivities.length} activities...</p>
-      {allActivities.map((activity) => {
-        return (
-          <div key={activity.id} className="activity-card">
-            <h4>{activity.name}</h4>
-            <p>Description: {activity.description}</p>
-          </div>
-        );
-      })}
+      <ActivitiesHeader
+        createWindowOpen={createWindowOpen}
+        setCreateWindowOpen={setCreateWindowOpen}
+        allActivities={allActivities}
+        setDisplayedActivities={setDisplayedActivities}
+        displayedActivities={displayedActivities}
+        myProfile={myProfile}
+      />
+      <ActivityCreate createWindowOpen={createWindowOpen} token={token} />
+      <div id="activities-list">
+        {displayedActivities.map((activity) => {
+          return (
+            <div key={activity.id} className="activity-card">
+              <h4>{activity.name}</h4>
+              <p>Description: {activity.description}</p>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
+
+export default Activities;
