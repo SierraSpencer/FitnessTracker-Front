9aa521fcd5319258188fa70a28eb37a8374d3e2d
Sierra Spencer <31259321+SierraSpencer@users.noreply.github.com>
2023-06-19 21:43:13 -0700

    Update Routines.jsx

--- src/components/Routines.jsx
+++ src/components/Routines.jsx
@@ -3,7 +3,7 @@ import { getAllRoutines, createRoutine } from '../api';
 import './Routines.css';
 import { useNavigate, useOutletContext } from 'react-router';
 import RoutinesHeader from './RoutinesHeader';
-import CreateRoutineForm from './CreateRoutineForm';
+//import CreateRoutineForm from './CreateRoutineForm';
 
 export default function Routines()
 {
